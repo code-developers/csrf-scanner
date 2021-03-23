47cfed30767c3925e173b05473211151d59930fc
krishpranav <krisna.pranav@gmail.com>
2021-03-23 16:49:21 +0530

    done changes

--- csrf.py
+++ csrf.py
@@ -10,3 +10,34 @@ def banner():
         ''' % (yellow, white, yellow, end))
 
 banner()
+
+try:
+    import concurrent.futures
+    try:
+        from fuzzywuzzy import fuzz, process
+    except:
+        import os
+        print ('%s fuzzywuzzy library is not installed, installing now.' % info)
+        os.system('pip3 install fuzzywuzzy')
+        print ('%s fuzzywuzzy has been installed, please restart Bolt.' % info)
+        quit()
+except:
+    print ('%s CSRF-SCANNER is not compatible with python 2. Please run it with python 3.' % bad)
+
+import argparse
+import json
+import random
+import re
+import statistics
+
+from core.entropy import isRandom
+from core.datanize import datanize
+from core.prompt import prompt
+from core.photon import photon
+from core.tweaker import tweaker
+from core.evaluate import evaluate
+from core.ranger import ranger
+from core.zetanize import zetanize
+from core.requester import requester
+from core.utils import extractHeaders, strength, isProtected, stringToBinary, longestCommonSubstring
+
